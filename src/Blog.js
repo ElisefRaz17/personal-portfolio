de4b6ce7589dbe3d6fb16886e3a6f2f7ae5bc966
Elise Frazier <efrazie7@uncc.edu>
2024-12-15 18:45:42 -0500

    removed conflicting dependency

--- src/Blog.js
+++ src/Blog.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { LinkedInEmbed } from "react-social-media-embed";
-import GitHubCards from "./projects/GitHubCards";
+// import GitHubCards from "./projects/GitHubCards";
 
 const Blog = () => {
   return (
     <section className="bg-primary text-orange-950 px-5 py-32" style={{paddingTop:'0px'}} id="blog">
       <div style={{ display: "flex", justifyContent: "center", gap:"2rem" }}>
-        <GitHubCards />
+        {/* <GitHubCards /> */}
         <LinkedInEmbed
           url="https://www.linkedin.com/embed/feed/update/urn:li:share:7038566552702513152"
           postUrl="https://www.linkedin.com/posts/elise-frazier-89b356180_thank-you-to-girls-who-code-at-unc-charlotte-activity-7174925448118718464-PNvs?utm_source=share&utm_medium=member_desktop"
